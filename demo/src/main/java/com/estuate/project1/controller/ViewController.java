6aab7619a815c68cac55489be6d647feef7fbae8
stark <tonystarkv9@gmail.com>
2025-01-17 21:17:34 +0530

    release_v1.0

--- demo/src/main/java/com/estuate/project1/controller/ViewController.java
+++ demo/src/main/java/com/estuate/project1/controller/ViewController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ViewController {
 
-    @RequestMapping(value = "/{path:[^\\.]*}") // Matches all paths except those with a dot (e.g., .css, .js)
+    @RequestMapping(value = "/{path:[^\\.]*}")
     public String redirect() {
         return "forward:/index.html";
     }
